dec8d8599df7262519623b2931935010c3807777
Tomas Pocius <pocius.tomastp@gmail.com>
2022-06-20 17:59:13 +0300

    not working. saving before new approach!

--- calculator.js
+++ calculator.js
@@ -1,6 +1,13 @@
-let sum = function (a, b) {
-    return a + b;
+let display = document.getElementById("numbers");
+let input = sessionStorage.getItem("input");
+let input2 = sessionStorage.getItem("input2");
+let operator = sessionStorage.getItem("operator");
+
+
+ let sum = function (a, b) {
+   return a + b;
 };
+
 
 let subtract = function (a, b) {
     return a - b;
@@ -14,16 +21,85 @@ let divide = function (a, b) {
     return a / b;
 };
 
-function operate (a, b) {
-    if (a + b) {
-        sum(a, b);
-    } else if (a - b) {
-        subtract(a, b); 
-    } else if (a * b) {
-        multiply(a, b);
-    } else if (a / b) {
-        divide(a, b);
+
+let operate = function () {
+    if (operator == "+") {
+    return sum(Number(input2), Number(input));
+    } else if (operator == "-") {
+    return subtract(Number(input2), Number(input));
+    } else if (operator == "*") {
+    return  multiply(Number(input2), Number(input));
+    } else if (operator == "/") {
+    return divide(Number(input2), Number(input));
     }
+    display.innerText
 }
 
-console.log(operate (2 + 3))
+
+
+document.getElementById("add").addEventListener("click", () => {
+    if (display.innerText == "+") {
+    sessionStorage.setItem("operator", display.innerText += "");
+    } else {
+    input2 = sessionStorage.setItem("input2", display.innerText += "");
+    };
+    display.innerText = "";
+    sessionStorage.setItem("operator", display.innerText = "+");
+       
+});
+
+document.getElementById("seven").addEventListener("click", () => {
+    if (display.innerText == "+" ||display.innerText == 0) {
+    display.innerText = display.innerText.slice(0, -1);
+    sessionStorage.setItem("input", display.innerText);
+    } 
+    sessionStorage.setItem("input", display.innerText += "7");
+
+});
+
+document.getElementById("eight").addEventListener("click", () => {
+    if (display.innerText == "+") {
+        display.innerText = display.innerText.slice(0, -1);
+        sessionStorage.setItem("input", display.innerText);
+        } 
+    sessionStorage.setItem("input", display.innerText += "8");
+   
+})
+
+document.getElementById("clear").addEventListener("click", () => {
+   display.innerText = display.innerText.slice(0, -1);
+   sessionStorage.setItem("input", display.innerText);
+   
+})
+
+document.getElementById("equal").addEventListener("click", () => {
+    
+    console.log(input);
+    console.log(input2);
+    console.log(operate())
+    
+})
+
+
+document.getElementById("allclear").addEventListener("click", () => {
+    display.innerText = "0";
+    sessionStorage.clear()
+    
+})
+
+
+
+display.innerText = operate()
+console.log(input)
+console.log(operator)
+console.log(input2)
+console.log(operate())
+
+
+
+
+
+
+
+
+
